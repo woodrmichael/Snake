67a952b286d40c2f3e92a6846ee7298cd5bf17b2
woodrmichael <woodm@msoe.edu>
2023-11-22 13:07:46 -0600

    GamePanel.java constructor finished

--- src/woodm/GamePanel.java
+++ src/woodm/GamePanel.java
@@ -7,26 +7,58 @@
  */
 package woodm;
 
-import javax.swing.JPanel;
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
+    private static final int SCREEN_WIDTH = 600;
+    private static final int SCREEN_HEIGHT = 600;
+    private static final int UNIT_SIZE = 25;
+    private static final int TOTAL_UNITS = SCREEN_WIDTH * SCREEN_HEIGHT / UNIT_SIZE;
+    private static final int DELAY = 75;
+    private static final int STARTING_BODY_PARTS = 6;
+    Timer timer;
+    Random generator;
+    private final int[] x;
+    private final int[] y;
+    private int bodyParts;
+    private int applesEaten;
+    private int appleX;
+    private int appleY;
+    private char direction;
+    private boolean running;
 
     public GamePanel() {
+        this.generator = new Random();
+        this.x = new int[TOTAL_UNITS];
+        this.y = new int[TOTAL_UNITS];
+        this.bodyParts = STARTING_BODY_PARTS;
+        this.direction = 'R';
+        this.running = false;
 
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        this.startGame();
     }
 
     public void startGame() {
 
     }
 
+    @Override
     public void paintComponent(Graphics g) {
 
     }
 
     public void draw(Graphics g) {
+
+    }
+
+    public void newApple() {
 
     }
 
