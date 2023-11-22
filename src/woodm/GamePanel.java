2e77cfe59c6cfc66bdc7daa7f5a45f28963c5c79
woodrmichael <woodm@msoe.edu>
2023-11-22 17:18:18 -0600

    snake moving working

--- src/woodm/GamePanel.java
+++ src/woodm/GamePanel.java
@@ -20,7 +20,7 @@ public class GamePanel extends JPanel implements ActionListener {
     private static final int DELAY = 75;
     private static final int STARTING_BODY_PARTS = 6;
     private Timer timer;
-    private Random generator;
+    private final Random generator;
     private final int[] x;
     private final int[] y;
     private int bodyParts;
@@ -59,35 +59,151 @@ public class GamePanel extends JPanel implements ActionListener {
 
     @Override
     public void paintComponent(Graphics g) {
-
+        super.paintComponent(g);
+        this.draw(g);
     }
 
+    /**
+     * Draws all components of the game.
+     * @param g Graphics object g used to draw everything needed for the game.
+     */
     public void draw(Graphics g) {
+        if(running) {
+            // Draws Lines across x-axis then y-axis to make a grid.
+            for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT); // vertical lines
+                g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE); // horizontal lines
+            }
 
+            g.setColor(Color.red);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+            for (int i = 0; i < this.bodyParts; i++) {
+                if (i == 0) { // Sets head of snake to a different color.
+                    g.setColor(Color.blue);
+                    g.fillRect(this.x[i], this.y[i], UNIT_SIZE, UNIT_SIZE);
+                } else {
+                    g.setColor(Color.green);
+                    g.fillRect(this.x[i], this.y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+        } else {
+            this.gameOver(g);
+        }
     }
 
+    /**
+     * Creates a new Apple in a random location in the grid.
+     */
     public void newApple() {
-
+        this.appleX = this.generator.nextInt(SCREEN_WIDTH / UNIT_SIZE) * UNIT_SIZE;
+        this.appleY = this.generator.nextInt(SCREEN_HEIGHT / UNIT_SIZE) * UNIT_SIZE;
     }
 
+    /**
+     * Moves the snake based on the direction it is facing.
+     */
     public void move() {
+        for(int i = this.bodyParts; i > 0; i--) {
+            this.x[i] = this.x[i - 1];
+            this.y[i] = this.y[i - 1];
 
+        }
+
+        switch(this.direction) {
+            case 'U':
+                this.y[0] = this.y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                this.y[0] = this.y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                this.x[0] = this.x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                this.x[0] = this.x[0] + UNIT_SIZE;
+                break;
+        }
     }
 
+    /**
+     * Checks to see if the snake has hit an apple.
+     */
     public void checkApple() {
+        if(this.x[0] == this.appleX && this.y[0] == this.appleY) {
+            this.bodyParts++;
+            this.applesEaten++;
+            this.newApple();
+        }
+
 
     }
 
+    /**
+     * Checks to see if the snake has hit itself or hit any of the borders.
+     * Stops the game if that has happened.
+     */
     public void checkCollisions() {
-
+        // Checks if head of snake collides with body.
+        for(int i = this.bodyParts; i > 0; i--) {
+            if(this.x[0] == x[i] && this.y[0] == this.y[i]) {
+                this.running = false;
+                break;
+            }
+        }
+        // Checks if head of snake touches left or right border.
+        if(this.x[0] < 0 || this.x[0] > SCREEN_WIDTH) {
+            this.running = false;
+        }
+        // Checks if head of snake touches top or bottom border.
+        if(this.y[0] < 0 || this.y[0] > SCREEN_HEIGHT) {
+            this.running = false;
+        }
+        if(!running) {
+            this.timer.stop();
+        }
     }
 
     public void gameOver(Graphics g) {
-
+        g.setColor(Color.red);
+        g.setFont(new Font(""))
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(this.running) {
+            this.move();
+            this.checkApple();
+            this.checkCollisions();
+        }
+        repaint();
+    }
 
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch(e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if(direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if(direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if(direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if(direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+            }
+        }
     }
 }
